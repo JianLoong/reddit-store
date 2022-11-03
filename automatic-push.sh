ba10380fb5caa8d24d32de24d6b5616ae5663b35
Automated Push <none@automated>
2022-11-03 19:49:04 +1100

    Updated

--- automatic-push.sh
+++ automatic-push.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/bash
 HOME=/home/pi
 cd /home/pi/reddit-store
+/home/pi/reddit-store/venv/bin/python /home/pi/reddit-store/subreddit.py
+/home/pi/reddit-store/venv/bin/python /home/pi/reddit-store/analytics.py
 git add .
 git commit -m "Updated"
 GIT_SSH_COMMAND='ssh -i ~/redditbot' git push 
