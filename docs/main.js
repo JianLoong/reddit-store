1cb96f7db452ea0db78bf012713ae360e945ea62
Jian Loong Liew <JianLoong@users.noreply.github.com>
2022-11-07 13:26:52 +1100

    Refactored

--- docs/main.js
+++ docs/main.js
@@ -1,26 +1,9 @@
 // Method to fetch the indexes
-
-const defaultStartUTC = () => {
-    const startOfToDay = new Date();
-    startOfToDay.setHours(0, 0, 0, 0);
-    const startUTC = startOfToDay.getTime() / 1000;
-    return startUTC;
-}
-
-const defaultEndUTC = () => {
-
-    const endOfToday = new Date();
-    endOfToday.setDate(endOfToday.getDate() + 1);
-    endOfToday.setHours(0, 0, 0, 0);
-    const endUTC = endOfToday.getTime() / 1000;
-    return endUTC;
-}
-
 const fetchIndex = ({
-    order = "hot",
-    startUTC = defaultStartUTC(),
-    endUTC = defaultEndUTC(),
-    noOfPost = 10
+    order,
+    startUTC,
+    endUTC,
+    noOfPost
 } = {}) => {
     fetch("./api/indexes/indexes.json" + '?' + Math.random())
         .then(response => {
@@ -33,14 +16,38 @@ const fetchIndex = ({
             return indexes;
         })
         .then(result => {
-            createDiv(result);
+            const submissionsDiv = document.getElementById("submissions");
+  
+            submissionsDiv.innerHTML = createDiv(result);
             for (let index of result)
                 fetchSubmission(index);
         })
         .catch(err => {
             //showErrorDiv("submissionLoading", err.getMessage(), true);
             //console.log(err);
-            setupDatePicker();
+            //setupDatePicker();
+        });
+}
+
+const fetchSubmission = (index) => {
+    showLoading(index + "_submissionLoading", true);
+
+    fetch("./api/submissions/" + index + ".json")
+        .then(response => {
+            if (response.ok)
+                return response.json();
+            throw new Error("Something went wrong");
+        })
+        .then(data => {
+            showLoading(index + "_submissionLoading", false);
+            const json = data;
+            let submission = json;
+            const submissionDiv = document.getElementById(submission.id + "_submission");
+            submissionDiv.innerHTML = buildSubmissions(submission)
+            processSentiments(index);
+        })
+        .catch(err => {
+            showErrorDiv("submissions", "Please try at another time.")
         });
 }
 
@@ -71,160 +78,6 @@ const sortIndexes = (data, startUTC, endUTC, noOfPost, order) => {
     return indexes;
 }
 
-const showErrorDiv = (id, errorMessage, isShown) => {
-
-    errorDiv = document.getElementById(id);
-
-    if (isShown == false) {
-        errorDiv.innerHTML = "";
-        return;
-    }
-    errorDiv.innerHTML = "";
-    htmlString = '<div class="alert alert-warning" role="alert">';
-    htmlString += errorMessage;
-    htmlString += '</div >'
-    errorDiv.innerHTML = htmlString;
-}
-
-const fetchSubmission = (index) => {
-    showLoading(index + "_submissionLoading", true);
-
-    fetch("./api/submissions/" + index + ".json")
-        .then(response => {
-            if (response.ok)
-                return response.json();
-            throw new Error("Something went wrong");
-        })
-        .then(data => {
-            showLoading(index + "_submissionLoading", false);
-            const json = data;
-            buildSubmissions(json);
-            processSentiments(index);
-        })
-        .catch(err => {
-            showErrorDiv("submissions", "Please try at another time.")
-        });
-}
-
-const showLoading = (id, isLoading, loadingMessage) => {
-    const postDiv = document.getElementById(id);
-    if (isLoading)
-        postDiv.classList.add("spinner-border");
-    else
-        postDiv.classList.remove("spinner-border");
-}
-
-const makeHTMLFromString = (str) => {
-    let text = str;
-    parsed = "<p>" + text + "</p>";
-    parsed = parsed.replace(/\r\n\r\n/g, "</p><p>").replace(/\n\n/g, "</p><p>");
-    parsed = parsed.replace(/\r\n/g, "<br />").replace(/\n/g, "<br />");
-    return parsed;
-}
-
-const createDiv = (indexes) => {
-    const submissionsDiv = document.getElementById("submissions");
-    let htmlString = "";
-    for (const index of indexes) {
-        htmlString += "<div class='card'><div class='card-body'>";
-        htmlString += "<div id =" + index + "_submissionLoading></div>";
-        htmlString += "<div id =" + index + "_submission></div>";
-        htmlString += "<div id =" + index + "_sentimentLoading></div>";
-        htmlString += "<div class='row' class='" + index + "'>"
-        htmlString += "<div class='col-md-4'>"
-
-        let afinnDivId = index + "_afinn";
-        htmlString += "<div class='' id =" + afinnDivId + " +></div>";
-
-        let cloudId = index + "_cloud";
-        htmlString += "<div id='" + cloudId + "'></div>";
-        htmlString += "</div>"
-        htmlString += "<div class='col-md-4'>"
-
-        let nrc = index + "_nrc";
-        htmlString += "<div class='' id =" + nrc + "></div>";
-        htmlString += "</div>";
-        let chart = index + "_chart";
-        htmlString += "<div class='col-md-4'>"
-        htmlString += "<canvas id='" + chart + "'width='' height='400'></canvas>";
-        htmlString += "</div>";
-        htmlString += "</div></div></div><p></p>";
-    }
-    submissionsDiv.innerHTML = htmlString;
-}
-
-const makeCloud = (id, words) => {
-    htmlString = "";
-    let cloudId = document.getElementById(id + "_cloud");
-
-    words = Object.keys(words);
-    let keys = Object.keys(words);
-    let values = Object.values(words);
-    let processedWords = [];
-
-    let sum = 0;
-
-    for (let i = 0; i < keys.length; i++) {
-        processedWords.push({
-            "text": keys[i],
-            "size": values[i]
-        })
-        sum += values;
-    }
-
-    // This computed width is used to draw the cloud so that the size is right.
-    const width = document.getElementById(id + "_cloud").getBoundingClientRect().width;
-
-    const draw = (words) => {
-        d3.select(cloudId).append("svg")
-            .attr("width", layout.size()[0])
-            .attr("height", layout.size()[1])
-            .append("g")
-            .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
-            .selectAll("text")
-            .data(words)
-            .enter().append("text")
-            .style("font-size", function (d) { return d.size + "px"; })
-            .style("font-family", "Impact")
-            .attr("text-anchor", "middle")
-            .attr("transform", function (d) {
-                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
-            })
-            .text(function (d) { return d.text; });
-    }
-
-    var layout = d3.layout.cloud()
-        .size([width, width])
-        .words(words.map(function (d) {
-            return { text: d, size: 10 + Math.random() * 90 };
-        }))
-        .padding(5)
-        .rotate(function () { return 0; })
-        .font("Impact")
-        .fontSize(function (d) { return d.size; })
-        .on("end", draw);
-
-    layout.start();
-
-}
-
-const buildSubmissions = (submission) => {
-    const submissionDiv = document.getElementById(submission.id + "_submission");
-    htmlString = "";
-    htmlString += "<h1 class='card-title'>" + submission.title + "</h1>";
-    htmlString += "<hr>"
-    htmlString += "<small>" + convertDate(submission.created_utc) + "</small>";
-    htmlString += "<p></p>"
-    htmlString += "" + makeHTMLFromString(submission.selftext) + "";
-
-    let link = "https://reddit.com/" + submission.permalink;
-
-    htmlString +=
-        "<p>View original post <a href='" + link + "' class='card-link'>here</a></p>";
-    htmlString += "<p>Number of replies <strong>" + submission.replies.length + "</strong></p>";
-    submissionDiv.innerHTML = htmlString;
-}
-
 const processSentiments = (id) => {
 
     showLoading(id + "_sentimentLoading", true);
@@ -241,8 +94,16 @@ const processSentiments = (id) => {
             const chartLocation = id + "_chart";
             const chart_data = [counts.nta_count, counts.yta_count, counts.esh, counts.nah_count, counts.info_count]
             buildChart(chartLocation, chart_data);
-            processNRC(id, response["emotion"]);
-            makeCloud(id, response["word_freq"]);
+
+            const location = id + "_nrc";
+            let nrcResultsDiv = document.getElementById(location);
+
+            const emotions = response["emotion"];
+            nrcResultsDiv.innerHTML = processNRC(emotions);
+            
+            const cloudLocation = id + "_cloud";
+            makeCloud(cloudLocation, response["word_freq"]);
+
         })
         .catch(err => {
             showLoading(id + "_sentimentLoading", false);
@@ -251,85 +112,24 @@ const processSentiments = (id) => {
 
 }
 
-const processNRC = (id, nrc) => {
-    const location = id + "_nrc";
-    let nrcResultsDiv = document.getElementById(location);
-    let tableHTML = '<table class="result-table table table-bordered table-striped mb-0">';
-    tableHTML += '<thead><tr><th>Emotion</th><th>Raw Emotion Score</th></tr></thead>';
-    tableHTML += "<tbody>";
-    tableHTML += "<tr><td>" + "Positive" + "</td><td>" + nrc.positive + "</td></tr>";
-    tableHTML += "<tr><td>" + "Negative" + "</td><td>" + nrc.negative + "</td></tr>";
-    tableHTML += "<tr><td>" + "Fear" + "</td><td>" + nrc.fear + "</td></tr>";
-    tableHTML += "<tr><td>" + "Anger" + "</td><td>" + nrc.anger + "</td></tr>";
-    tableHTML += "<tr><td>" + "Anticipation" + "</td><td>" + nrc.anticipation + "</td></tr>";
-    tableHTML += "<tr><td>" + "Trust" + "</td><td>" + nrc.trust + "</td><tr>";
-    tableHTML += "<tr><td>" + "Surprise" + "</td><td>" + nrc.surprise + "</td></tr>";
-    tableHTML += "<tr><td>" + "Sadness" + "</td><td>" + nrc.sadness + "</td></tr>";
-    tableHTML += "<tr><td>" + "Disgust" + "</td><td>" + nrc.disgust + "</td></tr>";
-    tableHTML += "<tr><td>" + "Joy" + "</td><td>" + nrc.joy + "</td></tr>";
-    tableHTML += "</tbody></table>"
-
-    htmlString = "<h5 class='card-title'><p class=text-center>" + "Results of NRC Emotion Lexicon Analysis" + "</p></h5>";
-
-    nrcResultsDiv.innerHTML = htmlString + tableHTML;
-}
-
-const processAfinn = (id, score) => {
-    const location = id + "_afinn";
-    let afinnResultsDiv = document.getElementById(location);
-    if (score < 0) {
-        afinnResultsDiv.innerHTML = '<div class="alert alert-warning" role="alert">Negative</div>';
-    }
-
-    if (score == 0) {
-        afinnResultsDiv.innerHTML = '<div class="alert alert-info" role="alert">Neutral</div>';
-    }
-    if (score > 0) {
-        afinnResultsDiv.innerHTML = '<div class="alert alert-success" role="alert">Positive</div>';
-    }
-    let tableHTML = '<table class="result-table table table-bordered table-striped mb-0"><thead><tr><th>Score</th></thead>';
-    afinnResultsDiv.innerHTML += tableHTML + "<tr><td>" + score + "</td></tr></table>";;
-}
-
-const convertDate = (dateInUTC) => {
-    const event = new Date(dateInUTC * 1000);
-    displayTime = event.toDateString() + " " + event.toTimeString();
-    return displayTime;
-}
-
-const buildChart = (id, dataArr) => {
-    const ctx = document.getElementById(id).getContext("2d");
-    const data = {
-        labels: [
-            "Not the A-hole",
-            "You’re the A-hole",
-            "Everyone sucks here",
-            "No A-holes here",
-            "Not Enough Info",
-        ],
-        datasets: [
-            {
-                data: dataArr,
-                backgroundColor: [
-                    "green",
-                    "red",
-                    "rgb(255, 205, 86)",
-                    "rgb(201, 203, 207)",
-                    "rgb(54, 162, 235)",
-                ],
-            },
-        ],
-    };
-    const myChart = new Chart(ctx, {
-        type: "pie",
-        data: data,
-        options: {
-
-        },
-    });
-};
-
 const config = () => {
+
+    const defaultStartUTC = () => {
+        const startOfToDay = new Date();
+        startOfToDay.setHours(0, 0, 0, 0);
+        const startUTC = startOfToDay.getTime() / 1000;
+        return startUTC;
+    }
+    
+    const defaultEndUTC = () => {
+    
+        const endOfToday = new Date();
+        endOfToday.setDate(endOfToday.getDate() + 1);
+        endOfToday.setHours(0, 0, 0, 0);
+        const endUTC = endOfToday.getTime() / 1000;
+        return endUTC;
+    }
+    
     const noOfPost = document.getElementById("numberOfPost").value;
     const sortOrder = document.getElementById("sortOrder").value;
     const dateInput = document.querySelector('input[type="date"]');
@@ -344,51 +144,45 @@ const config = () => {
     const endUTC = endOfDay.getTime() / 1000;
 
     return {
-        "startUTC": startUTC || defaultStartUTC,
-        "endUTC": endUTC || defaultEndUTC,
-        "noOfPost": noOfPost,
-        "order": sortOrder
+        "startUTC": startUTC || defaultStartUTC(),
+        "endUTC": endUTC || defaultEndUTC(),
+        "noOfPost": noOfPost || 10,
+        "order": sortOrder || "hot"
     }
 
 }
-const selectPost = () => {
-    const noOfPost = document.getElementById("numberOfPost");
-    noOfPost.addEventListener("change", (event) => {
-        fetchIndex(config());
-    })
-}
-
-const selectOrder = () => {
-    const sortOrder = document.getElementById("sortOrder");
-    sortOrder.addEventListener("change", (event) => {
-        fetchIndex(config());
-    })
-}
 
 const defaultPosts = () => {
-    fetchIndex();
+    fetchIndex(config());
 }
 
-
-
-const setupDatePicker = () => {
-    const dateControl = document.querySelector('input[type="date"]');
-    let max = new Date().toLocaleString('sv').split(" ")[0];
-    const minDate = new Date(Date.UTC(2022, 10, 01));
-    dateControl.value = max;
-    dateControl.setAttribute("max", max);
-    dateControl.setAttribute("min", minDate.toISOString().split("T")[0]);
-}
-
-const selectDate = () => {
+const setUpListeners = () => {
     const dateInput = document.querySelector('input[type="date"]');
     dateInput.addEventListener("blur", (event) => {
         fetchIndex(config());
-    })
+    });
+
+    const sortOrder = document.getElementById("sortOrder");
+    sortOrder.addEventListener("change", (event) => {
+        fetchIndex(config());
+    });
+
+    const noOfPost = document.getElementById("numberOfPost");
+    noOfPost.addEventListener("change", (event) => {
+        fetchIndex(config());
+    });
+
+    const setupDatePicker = () => {
+        const dateControl = document.querySelector('input[type="date"]');
+        let max = new Date().toLocaleString('sv').split(" ")[0];
+        const minDate = new Date(Date.UTC(2022, 10, 01));
+        dateControl.value = max;
+        dateControl.setAttribute("max", max);
+        dateControl.setAttribute("min", minDate.toISOString().split("T")[0]);
+    }
+
+    setupDatePicker();
 }
 
-setupDatePicker();
-selectDate();
-selectPost();
-defaultPosts()
-selectOrder();
+setUpListeners();
+defaultPosts();
---
+++ docs/util.js
@@ -0,0 +1,211 @@
+const makeHTMLFromString = (str) => {
+    let text = str;
+    parsed = "<p>" + text + "</p>";
+    parsed = parsed.replace(/\r\n\r\n/g, "</p><p>").replace(/\n\n/g, "</p><p>");
+    parsed = parsed.replace(/\r\n/g, "<br />").replace(/\n/g, "<br />");
+    return parsed;
+}
+
+const processNRC = (nrc) => {
+    let tableHTML = '<table class="result-table table table-bordered table-striped mb-0">';
+    tableHTML += '<thead><tr><th>Emotion</th><th>Raw Emotion Score</th></tr></thead>';
+    tableHTML += "<tbody>";
+    tableHTML += "<tr><td>" + "Positive" + "</td><td>" + nrc.positive + "</td></tr>";
+    tableHTML += "<tr><td>" + "Negative" + "</td><td>" + nrc.negative + "</td></tr>";
+    tableHTML += "<tr><td>" + "Fear" + "</td><td>" + nrc.fear + "</td></tr>";
+    tableHTML += "<tr><td>" + "Anger" + "</td><td>" + nrc.anger + "</td></tr>";
+    tableHTML += "<tr><td>" + "Anticipation" + "</td><td>" + nrc.anticipation + "</td></tr>";
+    tableHTML += "<tr><td>" + "Trust" + "</td><td>" + nrc.trust + "</td><tr>";
+    tableHTML += "<tr><td>" + "Surprise" + "</td><td>" + nrc.surprise + "</td></tr>";
+    tableHTML += "<tr><td>" + "Sadness" + "</td><td>" + nrc.sadness + "</td></tr>";
+    tableHTML += "<tr><td>" + "Disgust" + "</td><td>" + nrc.disgust + "</td></tr>";
+    tableHTML += "<tr><td>" + "Joy" + "</td><td>" + nrc.joy + "</td></tr>";
+    tableHTML += "</tbody></table>"
+
+    htmlString = "<h5 class='card-title'><p class=text-center>" + "Results of NRC Emotion Lexicon Analysis" + "</p></h5>";
+
+    return htmlString + tableHTML;
+}
+
+const processAfinn = (score) => {
+    let htmlString = "";
+    if (score < 0) {
+        htmlString = '<div class="alert alert-warning" role="alert">Negative</div>';
+    }
+
+    if (score == 0) {
+        htmlString = '<div class="alert alert-info" role="alert">Neutral</div>';
+    }
+    if (score > 0) {
+        htmlString = '<div class="alert alert-success" role="alert">Positive</div>';
+    }
+    let tableHTML = '<table class="result-table table table-bordered table-striped mb-0"><thead><tr><th>Score</th></thead>';
+    htmlString += tableHTML + "<tr><td>" + score + "</td></tr></table>";;
+
+    return htmlString;
+}
+
+const convertDate = (dateInUTC) => {
+    const event = new Date(dateInUTC * 1000);
+    displayTime = event.toDateString() + " " + event.toTimeString();
+    return displayTime;
+}
+
+const buildSubmissions = (submission) => {
+    htmlString = "";
+    htmlString += "<h1 class='card-title'>" + submission.title + "</h1>";
+    htmlString += "<hr>"
+    htmlString += "<small>" + convertDate(submission.created_utc) + "</small>";
+    htmlString += "<p></p>"
+    htmlString += "" + makeHTMLFromString(submission.selftext) + "";
+
+    let link = "https://reddit.com/" + submission.permalink;
+
+    htmlString +=
+        "<p>View original post <a href='" + link + "' class='card-link'>here</a></p>";
+    htmlString += "<p>Number of replies <strong>" + submission.replies.length + "</strong></p>";
+    
+    return htmlString;
+}
+
+const buildChart = (id, dataArr) => {
+    const ctx = document.getElementById(id).getContext("2d");
+    const data = {
+        labels: [
+            "Not the A-hole",
+            "You’re the A-hole",
+            "Everyone sucks here",
+            "No A-holes here",
+            "Not Enough Info",
+        ],
+        datasets: [
+            {
+                data: dataArr,
+                backgroundColor: [
+                    "green",
+                    "red",
+                    "rgb(255, 205, 86)",
+                    "rgb(201, 203, 207)",
+                    "rgb(54, 162, 235)",
+                ],
+            },
+        ],
+    };
+    const myChart = new Chart(ctx, {
+        type: "pie",
+        data: data,
+        options: {
+
+        },
+    });
+};
+
+const makeCloud = (cloudLocation, words) => {
+    htmlString = "";
+    let cloudId = cloudLocation;
+    words = Object.keys(words);
+    // let keys = Object.keys(words);
+    // let values = Object.values(words);
+    // let processedWords = [];
+
+    // let sum = 0;
+
+    // for (let i = 0; i < keys.length; i++) {
+    //     processedWords.push({
+    //         "text": keys[i],
+    //         "size": values[i]
+    //     })
+    //     sum += values;
+    // }
+
+    // This computed width is used to draw the cloud so that the size is right.
+    const width = document.getElementById(cloudId).getBoundingClientRect().width;
+    const cloudDiv = document.getElementById(cloudId);
+
+    const draw = (words) => {
+        d3.select(cloudDiv).append("svg")
+            .attr("width", layout.size()[0])
+            .attr("height", layout.size()[1])
+            .append("g")
+            .attr("transform", "translate(" + layout.size()[0] / 2 + "," + layout.size()[1] / 2 + ")")
+            .selectAll("text")
+            .data(words)
+            .enter().append("text")
+            .style("font-size", function (d) { return d.size + "px"; })
+            .style("font-family", "Impact")
+            .attr("text-anchor", "middle")
+            .attr("transform", function (d) {
+                return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
+            })
+            .text(function (d) { return d.text; });
+    }
+
+    var layout = d3.layout.cloud()
+        .size([width, width])
+        .words(words.map(function (d) {
+            return { text: d, size: 10 + Math.random() * 90 };
+        }))
+        .padding(5)
+        .spiral("rectangular")
+        .rotate(function () { return 0; })
+        .font("Impact")
+        .fontSize(function (d) { return d.size; })
+        .on("end", draw);
+
+    layout.start();
+
+}
+
+const showLoading = (id, isLoading, loadingMessage) => {
+    const postDiv = document.getElementById(id);
+    if (isLoading)
+        postDiv.classList.add("spinner-border");
+    else
+        postDiv.classList.remove("spinner-border");
+}
+
+const createDiv = (indexes) => {
+    let htmlString = "";
+    for (const index of indexes) {
+        htmlString += "<div class='card'><div class='card-body'>";
+        htmlString += "<div id =" + index + "_submissionLoading></div>";
+        htmlString += "<div id =" + index + "_submission></div>";
+        htmlString += "<div id =" + index + "_sentimentLoading></div>";
+        htmlString += "<div class='row' class='" + index + "'>"
+        htmlString += "<div class='col-md-4'>"
+
+        let afinnDivId = index + "_afinn";
+        htmlString += "<div class='' id =" + afinnDivId + " +></div>";
+
+        let cloudId = index + "_cloud";
+        htmlString += "<div id='" + cloudId + "'></div>";
+        htmlString += "</div>"
+        htmlString += "<div class='col-md-4'>"
+
+        let nrc = index + "_nrc";
+        htmlString += "<div class='' id =" + nrc + "></div>";
+        htmlString += "</div>";
+        let chart = index + "_chart";
+        htmlString += "<div class='col-md-4'>"
+        htmlString += "<canvas id='" + chart + "'width='' height='400'></canvas>";
+        htmlString += "</div>";
+        htmlString += "</div></div></div><p></p>";
+    }
+
+    return htmlString;
+}
+
+const showErrorDiv = (id, errorMessage, isShown) => {
+
+    errorDiv = document.getElementById(id);
+
+    if (isShown == false) {
+        errorDiv.innerHTML = "";
+        return;
+    }
+    errorDiv.innerHTML = "";
+    htmlString = '<div class="alert alert-warning" role="alert">';
+    htmlString += errorMessage;
+    htmlString += '</div >'
+    errorDiv.innerHTML = htmlString;
+}
