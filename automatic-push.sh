59ceaba8120988278f4112e372728e2c10023c35
Automated <noreply@automated.pus>
2023-09-25 20:40:40 +1000

    chore - migrated to antabax raspi

--- automatic-push.sh
+++ automatic-push.sh
@@ -1,15 +1,15 @@
-#!/usr/bin/bash
+#!/bin/bash
 # export GIT_TRACE_PACKET=1
 # export GIT_TRACE=1
 # export GIT_CURL_VERBOSE=1
-HOME=/home/pi
-cd /home/pi/reddit-store
-/home/pi/reddit-store/venv/bin/python /home/pi/reddit-store/subreddit.py
-/home/pi/reddit-store/venv/bin/python /home/pi/reddit-store/analytics.py
-/home/pi/reddit-store/venv/bin/python /home/pi/reddit-store/search.py
-/home/pi/reddit-store/venv/bin/python /home/pi/reddit-store/top.py
+HOME=/home/jian
+cd /home/jian/reddit-store
+/home/jian/reddit-store/venv/bin/python /home/jian/reddit-store/subreddit.py
+/home/jian/reddit-store/venv/bin/python /home/jian/reddit-store/analytics.py
+/home/jian/reddit-store/venv/bin/python /home/jian/reddit-store/search.py
+/home/jian/reddit-store/venv/bin/python /home/jian/reddit-store/top.py
 git add .
 git commit -m "Updated"
-# GIT_SSH_COMMAND='ssh -i /home/pi/redditbot' git push 
-date >> /home/pi/reddit-store/push.log 2>&1
-GIT_SSH_COMMAND="ssh -i ~/.ssh/id_rsa -F /dev/null" git push >> /home/pi/reddit-store/push.log 2>&1
+# GIT_SSH_COMMAND='ssh -i /home/jian/redditbot' git push 
+date >> /home/jian/reddit-store/push.log 2>&1
+GIT_SSH_COMMAND="ssh -i ~/.ssh/id_ed25519 -F /dev/null" git push >> /home/jian/reddit-store/push.log 2>&1
